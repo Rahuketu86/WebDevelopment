2e285119ade0fe23155a031ea85629d3d6a9d78c
Rahul Saraf <rahuketu86@gmail.com>
2013-05-09 11:32:49 +0200

    Initial commit for werkjeug

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,34 +1,56 @@
 import webapp2
 
 form ="""
-        <form method ='post' action="/testform">
-                <!-- action where a form submits to -->
-                <select name = 'q'>
-                    <option value='1'>one</option>
-                    <option value='2'>two</option>
-                    <option value='3'>three</option>
-                </select>
+        <form method ='post'>
+                What's your DOB?
                 <br>
-                <!-- Submits the form which by default submit to itself--></-->
-                <input type="submit" />
-                
-            </form>
-        """
+                <label>Month
+                    <input type="text" name = "month"/>
+                </label>
+                <label>DATE
+                    <input type="text" name = "date"/>
+                </label>
+                <label>YEAR
+                    <input type="text" name = "year"/>
+                </label>
+                <br>
+                <input type="submit" />   
+        </form>
+       """
+MONTHS=['January',
+        'Febuary',
+        'March',
+        'April'
+        'May'
+        'June'
+        'July'
+        'August'
+        'September'
+        'October'
+        'November'
+        'December']
+
+month_abbvs = dict((m[:3].lower(),m) for m in MONTHS)
+
 class HelloWebapp2(webapp2.RequestHandler):
     def get(self):
-#        self.response.headers['Content-Type'] = 'text/plain'
         self.response.out.write(form)
         
-class TestHandler(webapp2.RequestHandler):
     def post(self):
-#        self.response.headers['Content-Type'] = 'text/plain'
-#         q = self.request.get("q")
-#         self.response.out.write(q)
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.out.write(self.request)
+        self.response.out.write("Thanks for that totally valid date")
+        
+    def valid_Month(self,month):
+        returnval = None
 
-app = webapp2.WSGIApplication([('/', HelloWebapp2),
-                               ('/testform',TestHandler)],
+        if month:
+            if len(month)==3 and month.lower() in month_abbvs:
+                month = month_abbvs(month.lower())
+            elif month.capitalize() in MONTHS:
+                returnval = month.capitalize()
+        return returnval
+
+
+app = webapp2.WSGIApplication([('/', HelloWebapp2)],
                                      debug=True)
 
 def main():
